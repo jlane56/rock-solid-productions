87d09005cddb5545ad70446fa6624573533eec60
jlane56 <justin@jubileeremedia.com>
2026-06-02 20:43:39 -0500

    Let portal pages handle auth redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/crew(.*)", "/admin(.*)"]);
-
-export default clerkMiddleware(async (auth, request) => {
-  if (isProtectedRoute(request)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
